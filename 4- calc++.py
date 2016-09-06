f0e39313d4bb5fcc8cd109cba0dd269b85106bc9
Adam5-5 <adam5.5@wp.pl>
2016-09-06 13:09:30 +0200

    Delete 4- calc++.py

--- 4- calc++.py
+++
@@ -1,6 +0,0 @@
-#Pierwszy kalkulator!
-#Daniel Matkowski i Adrian Kiełczewski
-x=int(input('Podaj pierwsza wartosc: '))
-y=int(input('Podaj druga wartosc': ))
-
-print('\n Dodawanie:\t',x+y,'\n','Odejmowanie:\t',x-y,'\n','Dzielenie:\t',x/y,'\n','Mnozenie:\t',x*y)
